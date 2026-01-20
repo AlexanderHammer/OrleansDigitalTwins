98644c4cd0117de1633490c4073300798d08afde
Alexander Hammer <ahammer.dev@gmail.com>
2026-01-20 09:22:37 +0100

    added logger extension

--- MinimalOrleansTutorial/OrleansDeviceTwins/Grains/DeviceGrain.cs
+++ MinimalOrleansTutorial/OrleansDeviceTwins/Grains/DeviceGrain.cs
@@ -5,16 +5,14 @@ using System.Text.Json;
 
 namespace Orleans;
 
-public class DeviceGrain(ILogger<DeviceGrain> logger) : Grain, IDeviceGrain
+public partial class DeviceGrain(ILogger<DeviceGrain> logger) : Grain, IDeviceGrain
 {
   private Device? _device;
   private DateTime _lastReceived = DateTime.MinValue;
 
-  private readonly ILogger _logger = logger;
-
   public override async Task OnActivateAsync(CancellationToken cancellationToken)
   {
-    _logger.LogInformation("{IdentityString} activated\n", IdentityString);
+    LogIdentityStringActivated(IdentityString);
     this.RegisterGrainTimer(CheckConnectionStatus, new()
       {
         DueTime = TimeSpan.FromSeconds(15), 
@@ -25,43 +23,40 @@ public class DeviceGrain(ILogger<DeviceGrain> logger) : Grain, IDeviceGrain
 
   public override async Task OnDeactivateAsync(DeactivationReason reason, CancellationToken cancellationToken)
   {
-    _logger.LogInformation("{IdentityString} deactivated. Reason: {Serialize}", IdentityString, JsonSerializer.Serialize(reason));
+    LogIdentityStringDeactivatedReasonSerialize(IdentityString, JsonSerializer.Serialize(reason));
     await base.OnDeactivateAsync(reason, cancellationToken);
   }
 
   public Task UpdateDevice(Device? device)
   {
-    if (device is null)
+    if (_lastReceived == DateTime.MinValue)
     {
-      _logger.LogInformation("Device created {id}", IdentityString);
+      LogDeviceCreatedId(IdentityString);
     }
     
-    if (_device is not null)
+    if (_device is not null && _device.Equals(device))
     {
-      if (_device.Equals(device))
-      {
-        _lastReceived = DateTime.Now;
-        _logger.LogInformation("Device state unchanged: {id}", IdentityString);
-        return Task.FromResult(_device);
-      }
-      _logger.LogInformation("Device changed: {id}", IdentityString);
+      LogDeviceStateUnchangedId(IdentityString);
     }
-    
+    else
+    {
+      LogDeviceChangedId(IdentityString);
+    }
     
     _lastReceived = DateTime.Now;
     _device = device;
 
-    _logger.LogInformation("{Serialize}", JsonSerializer.Serialize(_device));
+    LogSerialize(JsonSerializer.Serialize(_device));
     return Task.FromResult(_device);
   }
   private Task CheckConnectionStatus(CancellationToken token)
   {
-    if (_device != null && DateTime.Now - _lastReceived >= TimeSpan.FromSeconds(30) && _device.Connected)
+    if (_lastReceived != DateTime.MinValue && DateTime.Now - _lastReceived >= TimeSpan.FromSeconds(30) && _device!.Connected)
     {
-      _logger.LogInformation("Device {IdentityString} disconnected", IdentityString);
+      LogDeviceIdentityStringDisconnected(IdentityString);
       _device = _device with { Connected = false };
     }
-    _logger.LogInformation(@"CheckConnectionStatus completed for {IdentityString}:\n{Serialize}\n", IdentityString, JsonSerializer.Serialize(_device));
+    LogCheckConnectionStatusCompletedForIdentity(IdentityString, JsonSerializer.Serialize(_device));
     return Task.CompletedTask;
   }
 }
---
+++ MinimalOrleansTutorial/OrleansDeviceTwins/Grains/DeviceGrain.logger.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Orleans
+{
+  public partial class DeviceGrain
+  {
+    [LoggerMessage(LogLevel.Information, "{identityString} activated\n")]
+    partial void LogIdentityStringActivated(string identityString);
+
+    [LoggerMessage(LogLevel.Information, "{identityString} deactivated. Reason: {serialize}")]
+    partial void LogIdentityStringDeactivatedReasonSerialize(string identityString, string serialize);
+    
+    [LoggerMessage(LogLevel.Information, "Device created {id}")]
+    partial void LogDeviceCreatedId(string id);
+
+    [LoggerMessage(LogLevel.Information, "Device changed: {id}")]
+    partial void LogDeviceChangedId(string id);
+
+    [LoggerMessage(LogLevel.Information, "Device state: {serialize}")]
+    partial void LogSerialize(string serialize);
+
+    [LoggerMessage(LogLevel.Information, "Device {identityString} disconnected")]
+    partial void LogDeviceIdentityStringDisconnected(string identityString);
+
+    [LoggerMessage(LogLevel.Information, @"CheckConnectionStatus completed for {identityString}:\n{serialize}\n")]
+    partial void LogCheckConnectionStatusCompletedForIdentity(string identityString, string serialize);
+
+    [LoggerMessage(LogLevel.Information, "Device state unchanged: {id}")]
+    partial void LogDeviceStateUnchangedId(string id);
+  }
+}
