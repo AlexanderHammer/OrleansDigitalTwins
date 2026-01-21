5b6a3f939dff476c08149b817583e8764f6b199f
Alexander Hammer <ahammer.dev@gmail.com>
2026-01-21 09:13:33 +0100

    added nullcheck for incoming http request

--- MinimalOrleansTutorial/OrleansDeviceTwins/DeviceFunction/DeviceFunctions.cs
+++ MinimalOrleansTutorial/OrleansDeviceTwins/DeviceFunction/DeviceFunctions.cs
@@ -9,22 +9,16 @@ using Microsoft.Extensions.Logging;
 
 namespace DeviceFunction.HttpFunction;
 
-public class DeviceFunctions
+public class DeviceFunctions(IGrainFactory factory)
 {
-  private readonly ILogger _logger;
-  private readonly IGrainFactory _factory;
-
-  public DeviceFunctions(IGrainFactory factory, ILoggerFactory loggerFactory)
-  {
-    _logger = loggerFactory.CreateLogger<DeviceFunctions>();
-    _factory = factory ?? throw new ArgumentNullException(nameof(factory));
-  }
+  private readonly IGrainFactory _factory = factory ?? throw new ArgumentNullException(nameof(factory));
 
   [Function(nameof(UpdateDevice))]
   public async Task<HttpResponseData> UpdateDevice([HttpTrigger(AuthorizationLevel.Function, "put")] HttpRequestData req)
   {
-
     UpdateDeviceRequest? data = JsonSerializer.Deserialize<UpdateDeviceRequest>(req.Body);
+    if(data == null) return req.CreateResponse(HttpStatusCode.BadRequest);
+    
     Device newDeviceState = new (data.Connected, data.Rssi, data.Tiers.ToList());
     IDeviceGrain? grain = _factory.GetGrain<IDeviceGrain>($"device@{data.DeviceId}");
     await grain.UpdateDevice(newDeviceState);
